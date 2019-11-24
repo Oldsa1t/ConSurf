3ca1c50c13e18158dae5c9800533d4340ecfed5c
Maxim <maximnik93@gmail.com>
2019-11-24 20:48:05 +0300

    Доработка макета + js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-window.addEventListener("DOMContentLoaded", function() {
+window.addEventListener("DOMContentLoaded", function () {
 
     "use strict";
     let dropdown = document.querySelectorAll(".dropdown"),
@@ -6,60 +6,30 @@ window.addEventListener("DOMContentLoaded", function() {
         dropdownLabel = document.querySelectorAll(".home__dropdown-label"),
         option = document.querySelectorAll(".home__option");
 
-            
-        for (let i = 0; i < dropdownLabel.length; i++) {
-            dropdownLabel[i].addEventListener("click", function() {
-                dropdownList[i].classList.toggle("home__dropdown-list--opened");
-            });
-        }
-
-        // document.addEventListener("click", function(event) {
-        //     let target = event.target,
-        //         currentDropdown = target.closest(".home__dropdown-label");
-
-        //     for (let i = 0; i < dropdownLabel.length; i++) {
-        //         if (!currentDropdown || i != currentDropdown) {
-        //             dropdown.classList.remove("home__dropdown-list--opened");
-        //         }
-        //     }
-
-
-        //     dropdownLabel.forEach((item) => {
-        //         if (!currentDropdown || item != currentDropdown) {
-        //             item.classList.remove("home__dropdown-list--opened");
-        //         }
-        //     });
-        // });
-
-        function selectOption() {
-            for (let i = 0; i < option.length; i++) {
-                option[i].addEventListener("click", function(event) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                    let value = option[i].getAttribute("data-value");
-                    let text = option[i].innerText;
-                    this.closest(".home__dropdown-label").value = value;
-                    this.closest(".home__dropdown-label").innerText = text;
-                    this.closest(".home__dropdown-list").classList.remove("home__dropdown-list--opened");
-                });
-            }
-        }
-
-        selectOption();
+    for (let i = 0; i < dropdownLabel.length; i++) {
+        dropdownLabel[i].addEventListener("click", function () {
+            dropdownList[i].classList.toggle("home__dropdown-list--opened");
+        });     
         
+        window.addEventListener("click", function (event) {
+            let currentDropdown = event.target.closest(".home__dropdown-label");
     
-                
-        // option.forEach((item) => {
-        //     item.addEventListener("click", function(event) {
-        //         event.preventDefault();
-        //         event.stopPropagation();
-        //         let value = this.getAttribute("data-value");
-        //         let text = this.innerText;
-        //         this.closest(".home__dropdown-label").value = value;
-        //         this.closest(".home__dropdown-label").innerText = text;
-        //         this.closest(".home__dropdown-list").classList.remove("home__dropdown-list--opened");
-        //     });
-        // });
-    
-            
+            if (!currentDropdown || dropdownLabel[i] != currentDropdown) {
+                dropdownList[i].classList.remove("home__dropdown-list--opened");
+            }
+        });
+    }
+
+    for (let i = 0; i < option.length; i++) {
+        option[i].addEventListener("click", function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            let value = option[i].getAttribute("data-value");
+            let text = option[i].innerText;
+            this.closest(".dropdown").querySelector(".home__dropdown-label").value = value;
+            this.closest(".dropdown").querySelector(".home__dropdown-label").innerText = text;
+            this.closest(".dropdown").querySelector(".home__dropdown-list").classList.remove("home__dropdown-list--opened");
+        });
+    }
+
 });
